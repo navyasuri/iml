1b504bffe621491b2cb4185b1b72ee03034a6fb8
Nav <navyasuri@nyu.edu>
2019-02-18 21:03:40 +0800

    class2 done, posenet

--- class2/js/script.js
+++ class2/js/script.js
@@ -1,4 +1,6 @@
 let video;
+let poseNet; // This would be the machine learning model
+let poses;
 
 
 function setup() {
@@ -6,9 +8,34 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide() // hides the html element
+
+    poseNet = ml5.poseNet(video, console.log("Model Loaded"));
+    poseNet.on('pose', (results) => {
+        poses = results; // store the trained data into poses 
+    });
 }
 
 function draw() {
     // image(img, x, y, w, h)
     image(video, 0, 0); // Video gives an image every frame, display at given coords
+
+    if (poses != undefined) {
+
+        for (let i = 0; i < poses.length; i++) { // Looping over all the poses
+            for (let j = 0; j < poses[i].pose.keypoints.length; j++) { // Looping over the keypoints in each pose
+                console.log(poses[i].pose.keypoints[j].part);
+                let x = poses[i].pose.keypoints[j].position.x; // x coord of part
+                let y = poses[i].pose.keypoints[j].position.y; // y coord of part
+
+                fill(255, 255, 0);
+                noStroke();
+                ellipse(x, y, 5, 5); // draw an ellipse at that point
+            }
+        }
+
+    }
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
 }
