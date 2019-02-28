545090630610f59d20cd8a6c02bc55a845e3eecf
Nav <navyasuri@nyu.edu>
2019-02-28 22:55:18 +0800

    emojis displayed on face

--- face/js/script.js
+++ face/js/script.js
@@ -1,7 +1,8 @@
 let video;
 let poseNet; // This would be the machine learning model
 let poses;
-let emojis;
+let emojis = [];
+let neutral;
 // import FaceExpressionNet from 'lib/faceapi'
 // let net = FaceExpressionNet();
 // console.log(net);
@@ -12,6 +13,10 @@ function setup() {
     background(255);
     video = createCapture(VIDEO);
     video.hide() // hides the html element
+
+    for(let i = 0; i<7; i++){
+        emojis.push(loadImage('img/'+i.toString()+'.png'));
+    }
 
     // poseNet = ml5.poseNet(video, console.log("Model Loaded"));
     // poseNet.on('pose', (results) => {
@@ -26,12 +31,15 @@ function setup() {
 }
 
 function draw() {
+    // background(0);
     // image(img, x, y, w, h)
-    image(video, 0, 0); // Video gives an image every frame, display at given coords
+    // image(video, 0, 0); // Video gives an image every frame, display at given coords
     // console.log(video.elt)
 
     const detectionsWithExpressions = faceapi.detectSingleFace(video.elt).withFaceExpressions()
         .then((detectionsWithExpressions) => {
+
+            console.log(detectionsWithExpressions);
 
             let bestExpr = "";
             let max = 0;
@@ -41,6 +49,8 @@ function draw() {
                 console.log("No face detected");
                 // bestExpr = "neutral";
                 // max = 1;
+                background(255);
+                image(video, 0, 0);
             }
             else {
                 let face = detectionsWithExpressions.detection;
@@ -52,6 +62,12 @@ function draw() {
                         bestImg = emojis[i];
                     }
                 }
+
+                let small = Math.max(face.box.width, face.box.height);
+                background(255);
+                image(video, 0, 0);
+                image(bestImg, face.box.x, face.box.y, small, small);
+                // console.log(neutral, face.box.x, face.box.y, small, small);
                 console.log(bestExpr);
             }
                 
