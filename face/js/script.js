ec0ce04cd2ffed4a64a7591d83c4f958ba999c1b
Nav <navyasuri@nyu.edu>
2019-02-28 21:40:46 +0800

    face expressions detected, now to display emojis

--- face/js/script.js
+++ face/js/script.js
@@ -1,22 +1,25 @@
 let video;
 let poseNet; // This would be the machine learning model
 let poses;
+let emojis;
 // import FaceExpressionNet from 'lib/faceapi'
 // let net = FaceExpressionNet();
 // console.log(net);
 
 
 function setup() {
-    createCanvas(640, 480);
-
+    createCanvas(1280, 720);
+    background(255);
     video = createCapture(VIDEO);
-    // video.hide() // hides the html element
+    video.hide() // hides the html element
 
     // poseNet = ml5.poseNet(video, console.log("Model Loaded"));
     // poseNet.on('pose', (results) => {
     //     poses = results; // store the trained data into poses 
     // });
-    
+
+    faceapi.load
+    faceapi.loadSsdMobilenetv1Model('/models')
     faceapi.loadFaceExpressionModel('/models');
     console.log(faceapi.nets)
 
@@ -25,7 +28,35 @@ function setup() {
 function draw() {
     // image(img, x, y, w, h)
     image(video, 0, 0); // Video gives an image every frame, display at given coords
+    // console.log(video.elt)
 
+    const detectionsWithExpressions = faceapi.detectSingleFace(video.elt).withFaceExpressions()
+        .then((detectionsWithExpressions) => {
+
+            let bestExpr = "";
+            let max = 0;
+
+
+            if (detectionsWithExpressions == undefined) {
+                console.log("No face detected");
+                // bestExpr = "neutral";
+                // max = 1;
+            }
+            else {
+                let face = detectionsWithExpressions.detection;
+                let exprs = detectionsWithExpressions.expressions;
+                for (let i = 0; i < exprs.length; i++) {
+                    if (exprs[i].probability > max) {
+                        max = exprs[i].probability;
+                        bestExpr = exprs[i].expression;
+                        bestImg = emojis[i];
+                    }
+                }
+                console.log(bestExpr);
+            }
+                
+
+            });
 
 
     // if (poses != undefined) {
