f4c2628ba753f3861a3f4485ee2796160230c819
Nav <navyasuri@nyu.edu>
2019-02-28 23:06:10 +0800

    Added multiple face detection

--- face/js/script.js
+++ face/js/script.js
@@ -14,8 +14,8 @@ function setup() {
     video = createCapture(VIDEO);
     video.hide() // hides the html element
 
-    for(let i = 0; i<7; i++){
-        emojis.push(loadImage('img/'+i.toString()+'.png'));
+    for (let i = 0; i < 7; i++) {
+        emojis.push(loadImage('img/' + i.toString() + '.png'));
     }
 
     // poseNet = ml5.poseNet(video, console.log("Model Loaded"));
@@ -36,43 +36,46 @@ function draw() {
     // image(video, 0, 0); // Video gives an image every frame, display at given coords
     // console.log(video.elt)
 
-    const detectionsWithExpressions = faceapi.detectSingleFace(video.elt).withFaceExpressions()
-        .then((detectionsWithExpressions) => {
+    const allFaces = faceapi.detectAllFaces(video.elt).withFaceExpressions()
+        .then((allFaces) => {
 
-            console.log(detectionsWithExpressions);
+        background(255);
+        image(video, 0, 0)
+            for (var detectionsWithExpressions of allFaces) {
+                console.log(allFaces);
 
-            let bestExpr = "";
-            let max = 0;
+                let bestExpr = "";
+                let max = 0;
 
 
-            if (detectionsWithExpressions == undefined) {
-                console.log("No face detected");
-                // bestExpr = "neutral";
-                // max = 1;
-                background(255);
-                image(video, 0, 0);
-            }
-            else {
-                let face = detectionsWithExpressions.detection;
-                let exprs = detectionsWithExpressions.expressions;
-                for (let i = 0; i < exprs.length; i++) {
-                    if (exprs[i].probability > max) {
-                        max = exprs[i].probability;
-                        bestExpr = exprs[i].expression;
-                        bestImg = emojis[i];
+                if (detectionsWithExpressions == undefined) {
+                    console.log("No face detected");
+                    // bestExpr = "neutral";
+                    // max = 1;
+                    // background(255);
+                    // image(video, 0, 0);
+                }
+                else {
+                    let face = detectionsWithExpressions.detection;
+                    let exprs = detectionsWithExpressions.expressions;
+                    for (let i = 0; i < exprs.length; i++) {
+                        if (exprs[i].probability > max) {
+                            max = exprs[i].probability;
+                            bestExpr = exprs[i].expression;
+                            bestImg = emojis[i];
+                        }
                     }
+
+                    let small = Math.max(face.box.width, face.box.height);
+                    
+                    image(bestImg, face.box.x, face.box.y, small, small);
+                    // console.log(neutral, face.box.x, face.box.y, small, small);
+                    console.log(bestExpr);
                 }
 
-                let small = Math.max(face.box.width, face.box.height);
-                background(255);
-                image(video, 0, 0);
-                image(bestImg, face.box.x, face.box.y, small, small);
-                // console.log(neutral, face.box.x, face.box.y, small, small);
-                console.log(bestExpr);
-            }
-                
 
-            });
+            }
+        });
 
 
     // if (poses != undefined) {
