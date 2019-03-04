2ae068e02c7dcb94ea341fb887d7a8bf5ae1a650
Nav <navyasuri@nyu.edu>
2019-03-04 21:01:19 +0800

    In class exercise

---
+++ 4class/fashion.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+fashion = keras.datasets.fashion_mnist
+
+# Get the data from the data set
+(train_images, train_labels), (test_images, test_labels) = fashion.load_data()
+
+# Check sizes'
+print(len(train_labels), train_images.shape)
+print(len(test_labels), test_images.shape)
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+index = np.random.randint(0, 60000)
+# imgplot = plt.imshow(train_images[index])
+# print("index", index, "label", class_names[train_labels[index]])
+# plt.show()
+
+# regularize each pixel to map from grayscale (0, 255) to (0, 1) [just scaling the data]
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+# Forward Propagation
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)), 
+    keras.layers.Dense(128, activation=tf.nn.relu), 
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam', 
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+predictions = model.predict(test_images)
+random_image = np.random.randint(0, 10000)
+prediction = predictions[random_image]
+
+print(prediction)
+
+pred_label = np.argmax(prediction)
+print(class_names[pred_label])
+plt.imshow(test_images[pred_label])
+plt.show()
