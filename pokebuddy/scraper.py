3b3d03d256078cc243c0508764ace507794d320f
Nav <navyasuri@nyu.edu>
2019-03-21 23:21:32 +0800

    Starting to scrape data for descriptions

---
+++ pokebuddy/scraper.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://pokemondb.net/pokedex/ivysaur")
+soup = BeautifulSoup(response.text, "html.parser")
+
+pe = soup.find("h2", string="Pokédex entries")
+entries_div = pe.find_next_sibling("div", class_="resp-scroll")
+tbody = list(entries_div.children)[0]
+types = []
+para = []
+# print(type(tbody))
+for row in tbody.children:
+    myrow= list(row.children)
+    th, td = myrow[0], myrow[1]
+    para.append(td.contents)
+    print(th, td, "ok")
