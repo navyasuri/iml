77efd7cc5bb282649c13f41961e02b32cd02f244
Nav <navyasuri@nyu.edu>
2019-03-25 17:47:32 +0800

    File restructuring

--- pokebuddy/pyd2v/classify.py
+++ pokebuddy/pyd2v/classify.py
@@ -3,7 +3,7 @@ from nltk import word_tokenize
 
 d2v = Doc2Vec.load("d2v.model")
 
-string = "drk pokemon hunting in the shadowss"
+string = "green pokemon with a seed on its back"
 x=d2v.infer_vector(word_tokenize(string.lower()),steps=50, alpha=0.025)
 print(x)
 
