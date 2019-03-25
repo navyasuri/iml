5b9effd5c379fa6f9412098527c6a1ca91e4c888
Nav <navyasuri@nyu.edu>
2019-03-25 14:42:18 +0800

    Temporarily move to name tags to see whats going on. Move to number after integrating web app

--- pokebuddy/classify.py
+++ pokebuddy/classify.py
@@ -3,7 +3,7 @@ from nltk import word_tokenize
 
 d2v = Doc2Vec.load("d2v.model")
 
-string = "a pokemon that has a seed on its back"
+string = "drk pokemon hunting in the shadowss"
 x=d2v.infer_vector(word_tokenize(string.lower()),steps=50, alpha=0.025)
 print(x)
 
