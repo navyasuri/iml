1710b1e7d1d162f19aaa32a96bf1a50eda4b7155
Nav <navyasuri@nyu.edu>
2019-03-25 18:03:29 +0800

    Started making a flask boilerplate

--- pokebuddy/pyd2v/classify.py
+++ pokebuddy/pyd2v/classify.py
@@ -3,7 +3,7 @@ from nltk import word_tokenize
 
 d2v = Doc2Vec.load("d2v.model")
 
-string = "green pokemon with a seed on its back"
+string = ""
 x=d2v.infer_vector(word_tokenize(string.lower()),steps=50, alpha=0.025)
 print(x)
 
