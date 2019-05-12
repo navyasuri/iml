75dcd84ac83a22b09770c57dcf068030e5b8ad9b
Nav <navyasuri@nyu.edu>
2019-05-12 19:51:31 +0800

    almost complete, finishing touches

--- repictionary/app.py
+++ repictionary/app.py
@@ -24,7 +24,7 @@ current_desc = ""
 
 # What to do when user goes to default route
 @app.route('/')
-def show_page(): # The function name can be anything
+def welcome(): # The function name can be anything
 
     # You can just return default text 
     # return "Hello. Welcome to default page"
@@ -45,11 +45,11 @@ def eval_and_display():
     # If we come from GameOps, set our parameters accordingly
     
     if 'fromops' in request.form:
-        global player1, player2
+        global player1, player2, rounds
         player1 = request.form['p1name']
         player2 = request.form['p2name']
         current_player = player1
-
+        rounds = int(request.form['numrounds'])*2
         # Display the player1 caption page
         return_page = "desc.html"
         return render_template(return_page, player=current_player)
@@ -57,6 +57,7 @@ def eval_and_display():
     # If we come from desc, then we should generate image
     if 'from_desc' in request.form:
         caption = request.form['caption']
+        global current_desc
         current_desc = caption
         # caption player is the one who provided caption to generate image
         caption_player = request.form['d_player']
@@ -70,13 +71,21 @@ def eval_and_display():
         generator.wait()
 
         return render_template('guess.html', player=next_player)
+    
+    if 'from_prev' in request.form:
+        
+        prev_guesser= request.form['guesser']
+        next_captioner = player1
+        if prev_guesser == player2:
+            next_captioner = player2
+        print(prev_guesser, next_captioner, player1_score, player2_score)
+        return render_template("desc.html", player=next_captioner)
 
     # Read form and decide parameters, path and image
 
-
-
-@app.route('/result')
+@app.route('/result', methods=['POST'])
 def result_and_next():
+    global current_round, player1_score, player2_score
     current_round+=1
     next_page=""
     if current_round == rounds:
@@ -85,12 +94,34 @@ def result_and_next():
         next_page="desc"
 
     guess = request.form['guessed']
+    guesser = request.form['guesser']
     
     score_value = score.score_sentences(current_desc, guess)
-    return render_template('result.html', next_page=next_page, score_val=score_value)
+    score_value = round(100*score_value, 2)
+    if guesser == player1:
+        player1_score += score_value
+    else: player2_score += score_value
+
+    print(current_round, current_desc)
+    return render_template('result.html', 
+    next_page=next_page, 
+    score_val=score_value, 
+    correct=current_desc, 
+    guess=guess, 
+    scoreof=guesser)
 
 
 @app.route('/end')
 def final_scoring():
-
-    return render_template("scoring.html", player1score=player2_score, player2_score=player2_score)
+    if player1_score>player2_score:
+        winner=player1
+    elif player2_score<player1_score:
+        winner=player2
+    else:
+        winner="Nobody! It's a tie"
+    return render_template("final.html", 
+    player1=player1,
+    player2=player2,
+    player1score=player1_score, 
+    player2_score=player2_score, 
+    winner=winner)
